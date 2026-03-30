81cc4db03fe492343c125f6ef9e60dc131ef214f
Data-MaSTeRR <hyunwoo8504@gmail.com>
2026-03-30 17:14:59 +0900

    Feat: Add Profiler class with sorting functionality and result display

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO : Profiler 실행해보기
+        Profiler profiler = new Profiler();
+
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(5, 2, 4, 1, 1, 3, 9));
+
+        // Input Data 세팅
+        profiler.setData(input);
+        // Sort Lib 세팅
+        profiler.setLib(new SelectionSort());
+        // 정렬
+        profiler.runLib();
+        // 결과 출력
+        profiler.showResult();
     }
 }
--- src/main/java/org/example/Profiler.java
+++ src/main/java/org/example/Profiler.java
@@ -1,5 +1,39 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Profiler {
-    // TODO : Profiler 구현
+
+    private SortServiceLib sortServiceLib;
+    private SortType sortType;
+
+    private ArrayList<Integer> input;
+
+    private int changeCnt;
+
+    public void setData(ArrayList<Integer> input) {
+        this.input = input;
+    }
+
+    public void setLib(SortServiceLib sortServiceLib) {
+        this.sortServiceLib = sortServiceLib;
+    }
+
+    public void runLib() {
+
+        if (sortServiceLib == null) {
+            throw new NullPointerException("SortServiceLib is null");
+        }
+
+        sortServiceLib.sort(input);
+        changeCnt = sortServiceLib.getChangeCnt();
+        sortType = sortServiceLib.getSortType();
+    }
+
+    public void showResult() {
+        System.out.println(sortType + " 테스트");
+        System.out.println(Arrays.toString(input.toArray()));
+        System.out.println("swap횟수: " + changeCnt);
+    }
 }
